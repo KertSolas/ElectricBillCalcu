fbbff7be2ca4a12e030dcc0366ad40d2ec323b93
Kert Solas <kertsolas@gmail.com>
2025-01-28 09:24:17 +0800

    updated vercel.json

--- server/server.js
+++ server/server.js
@@ -23,5 +23,6 @@ app.get('/', (req, res) => res.send('Hello World!'));
 
 // Start Server
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
